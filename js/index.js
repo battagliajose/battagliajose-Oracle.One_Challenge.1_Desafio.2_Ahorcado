5b70f27b58d8b689e7bf14b8982e5c116a81e6c6
battagliajose <battagliajose@gmail.com>
2022-06-02 14:48:50 -0300

    Agregada Vista inicial de Canvas

--- js/index.js
+++ js/index.js
@@ -1,8 +1,28 @@
+var btnIniciarJuego = document.querySelector("#btnIniciarJuego");
 var btnAgregarPalabra = document.querySelector("#btnAgregarPalabra");
+
 var mainDiv = document.querySelector("#mainDiv");
 var agregarPalabraDiv = document.querySelector("#agregarPalabraDiv");
+var juegoDiv = document.querySelector("#juegoDiv");
 
 var btnCancelarPalabra = document.querySelector("#btnCancelarPalabra");
+
+var canvas = document.querySelector("#canvas");
+var pincel = canvas.getContext("2d");
+var canvasW = canvas.scrollWidth;
+var canvasH = canvas.scrollHeight;
+
+btnIniciarJuego.addEventListener("click", function(){
+    mainDiv.classList.add("oculta");
+    juegoDiv.classList.remove("oculta");
+    
+    pincel.fillStyle = 'rgb(200, 0, 0)';
+    pincel.fillRect(10, 10, 50, 50);
+    pincel.fillRect(10, 10, 50, 50);
+    pincel.fillRect(20, 20, 50, 50);
+
+    alert(canvasW + " - " + canvasH);
+});
 
 btnAgregarPalabra.addEventListener("click", function(){
     mainDiv.classList.add("oculta");
