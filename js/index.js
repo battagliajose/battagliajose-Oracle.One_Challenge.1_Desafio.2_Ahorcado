e246f2955011d29c412bb988f870b3ece24ac139
battagliajose <86583581+battagliajose@users.noreply.github.com>
2022-06-05 01:17:53 -0300

    Agregado de funciones dibujarGuiones(), getPalabra() y rnd()

--- js/index.js
+++ js/index.js
@@ -20,6 +20,8 @@ window.addEventListener("resize", resizeCanvas);
 /*InputText Nueva Palabra*/
 var txtNuevaPalabra = document.querySelector("#txtNuevaPalabra");
 
+var palabra;
+
 txtNuevaPalabra.addEventListener("input", function(){
     beep();
 });
@@ -27,13 +29,39 @@ txtNuevaPalabra.addEventListener("input", function(){
 btnIniciarJuego.addEventListener("click", function(){
     mainDiv.classList.add("oculta");
     juegoDiv.classList.remove("oculta");
+    palabra = getPalabra();
     resizeCanvas();
     draw();
+
 });
 
 function draw() {
     context.fillStyle = 'rgb(200, 0, 0)';
     context.fillRect(canvasW/10, canvasH/10, canvasW/10, canvasH/10);
+
+    console.log(palabra);
+    dibujarGuiones(palabra);
+}
+
+function dibujarGuiones(palabra) {
+    var cantidadGuiones = palabra.length;
+
+    context.fillStyle = "black";
+    context.beginPath();
+    
+    let posY = canvasH * 0.9;
+    let posX = canvasW / cantidadGuiones;
+
+    context.lineWidth = canvasH *0.01;
+
+    guionWidth = canvasW / cantidadGuiones;
+
+    for (i = 0; i < cantidadGuiones; i++) {
+        context.moveTo( i * guionWidth, posY);
+        context.lineTo( (i+1) * guionWidth -10, posY);
+        context.stroke();
+    }
+
 }
 
 function resizeCanvas() {
@@ -73,7 +101,7 @@ function beep() {
     var oscillator = context.createOscillator();
     
     oscillator.type = "sine";
-    oscillator.frequency.value = Math.round(Math.random()*800+100);
+    oscillator.frequency.value = rnd(100,800);
     oscillator.connect(context.destination);
     
     oscillator.start(); 
@@ -82,4 +110,15 @@ function beep() {
     setTimeout(function () {
         oscillator.stop();
     }, 50);   
+}
+
+function getPalabra(){
+    var palabras = ["POCHOCLO","MANZANA","PAJARO","AVION","PLATO", "CAMARA", "ARBOL", "COMPUTADORA", "LIBRO", "ESCRITORIO"]
+    
+    return palabras[rnd(0, palabras.length)];
+}
+
+//Devuelve un valor aleatorio entre valMin y ValMax.
+function rnd(valMin, valMax) {
+    return Math.round(Math.random()*(valMax-valMin)+valMin);
 }
